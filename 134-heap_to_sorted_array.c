15e6048722d8f11acaac56b6a19399f5371f2eb7
Augusyyy <8851@holbertonstudents.com>
2024-04-14 01:07:28 +1000

    140new

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,6 +1,24 @@
 #include "binary_trees.h"
 #include<stdlib.h>
 /**
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the size of.
+ *
+ * Return: The size of the tree.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree)
+	{
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
+}
+/**
  * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of
  * integers
  * @heap: pointer to the root node of the heap to convert
